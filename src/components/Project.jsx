a2ea9fddbfb9335eb9f8a26945ce6a745b0ac50f
nathan <kvngnathan8420@gmail.com>
2023-06-12 05:03:54 +0000

    Add projectDetail page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
+//components
+import Modal from "./Modal";
+import Button from "./Button";
+
+//
+import { Link } from "react-router-dom";
 
 const Project = ({ project }) => {
+
 
   return (
 
@@ -9,7 +16,7 @@ const Project = ({ project }) => {
 
 <div className="grad w-full h-[280px] overflow-hidden rounded-[8px] relative group flex  flex-col items-center justify-center">
         {/*badge*/}
-        { project.created > "2023-04-20" ? (
+        { project.created_at > "2023-04-20" ? (
           <div
             className="absolute bg-accent text-primary top-4 right-4 px-2 text-[12px]
           rounded-full upppercase font-extrabold z-10"
@@ -33,22 +40,24 @@ const Project = ({ project }) => {
         <div className="px-6 pb-6 flex flex-col items-center justify-center">
           {/* Project name*/}
           <div className=" text-2xl md:text-xl font-semibold text-accent capitalized uppercase mb-10 ">
-            {project.field.substring(0, 18)}
+            {project.name.substring(0, 18)}
           </div>
 
 
           {/* Project button*/}
           <div className="mb-3 lg:mb-9 w-full mx-auto flex justify-center">
-              {project.value}
+          <Link className="btn btn-accent text-[12px] font-semibold max-w-[180px]" to={`/projects/${project.name}`}>
+            Open Project
+        </Link>
           </div>
         </div>
 
         <div  className="w-full ps-2 text-zinc-500 ">
-          created - {project.created}
+          created - {project.created_at}
         </div>
       </div>
       
-        
+      
       </>
 
     
